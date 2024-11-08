11b26a15ea8f5115da6ee980d15c8481d0ee981a
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2024-11-08 00:12:19 -0300

    Update codigos.js

--- revisao-responsivo/js/codigos.js
+++ revisao-responsivo/js/codigos.js
@@ -1,1 +1,27 @@
-alert("Olá!");
+let ac = document.getElementById("lampAc");
+let ap = document.getElementById("lampAp");
+
+ac.addEventListener("click", liga);
+ap.addEventListener("click", desliga);
+
+function liga(){
+    document.getElementById("lampada").src="./img/acesa.png";
+}
+
+function desliga(){
+    document.getElementById("lampada").src="./img/apagada.png";
+}
+
+let so = document.getElementById("some");
+let apr = document.getElementById("aparece");
+
+some.addEventListener("click", someL);
+apr.addEventListener("click", apareceL);
+
+function someL(){
+    document.getElementById("lampada").style.display = "none";
+}
+
+function apareceL(){
+    document.getElementById("lampada").style.display = "block";
+}
